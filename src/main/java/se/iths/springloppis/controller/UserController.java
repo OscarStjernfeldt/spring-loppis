819378628053c82bbcaf48f29af1f4e830864bc8
Oscar Stjernfeldt <oscarstjernfeldt@hotmail.com>
2022-03-03 15:46:43 +0100

    Add Logger to UserController.java

--- src/main/java/se/iths/springloppis/controller/UserController.java
+++ src/main/java/se/iths/springloppis/controller/UserController.java
@@ -1,5 +1,7 @@
 package se.iths.springloppis.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,6 +21,7 @@ import java.util.Optional;
 public class UserController {
 
     UserService userService;
+    Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -27,6 +30,14 @@ public class UserController {
     @PostMapping()
     public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user) {
         UserEntity createdUser = userService.createUser(user);
+
+        //För demo purpose
+        logger.trace("Vi loggar på TRACE-nivå");
+        logger.debug("Vi loggar på DEBUG-nivå");
+        logger.info("Vi loggar på INFO-nivå");
+        logger.warn("Vi loggar på WARNING-nivå");
+        logger.error("Vi loggar på ERROR-nivå");
+
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
